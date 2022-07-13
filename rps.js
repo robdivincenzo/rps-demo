ffd445e03016184538fe7630c923d23708572c3e
Rob DiVincenzo <rob.divincenzo@gmail.com>
2022-07-12 22:39:47 -0400

    Create game
    Create game loop with prompt for each round
    Modify conditionals to switch in order to easily account for invalid entries.

--- rps.js
+++ rps.js
@@ -21,37 +21,47 @@ function playRound(playerSelection, computerSelection) {
         return "Draw!";
     }
 
-    //Rock conditions
-    if( playerSelection == "rock" ) {
-        if( computerSelection == "paper") {
-            return "You Lose! Paper beats Rock";
-        } else if( computerSelection == "scissors" ) {
-            return "You Win! Rock beats Scissors";
-        }
+    switch( playerSelection ){
+        //Rock conditions
+        case "rock":
+            if( computerSelection == "paper") {
+                return "You Lose! Paper beats Rock";
+            } else if( computerSelection == "scissors" ) {
+                return "You Win! Rock beats Scissors";
+            }    
+            break;
+        //Paper conditions
+        case "paper":
+            if( computerSelection == "rock") {
+                return "You Win! Paper beats Rock";
+            } else if( computerSelection == "scissors" ) {
+                return "You Lose! Scissors beats Paper";
+            }    
+            break;
+        //Scissors conditions
+        case "scissors":
+            if( computerSelection == "rock") {
+                return "You Lose! Rock beats Scissors";
+            } else if( computerSelection == "paper" ) {
+                return "You Win! Scissors beats Paper";
+            }    
+            break;
+        //invalid
+        default:
+            return "Invalid selection. Try again.";
+            break;
     }
-    
-    //Paper conditions
-    if( playerSelection == "paper" ) {
-        if( computerSelection == "rock") {
-            return "You Win! Paper beats Rock";
-        } else if( computerSelection == "scissors" ) {
-            return "You Lose! Scissors beats Paper";
-        }
-    }
-
-    //Scissors conditions
-    if( playerSelection == "scissors" ) {
-        if( computerSelection == "rock") {
-            return "You Lose! Rock beats Scissors";
-        } else if( computerSelection == "paper" ) {
-            return "You Win! Scissors beats Paper";
-        }
-    }
-
 
   }
    
-  const playerSelection = "rock";
-  const computerSelection = computerPlay();
-  console.log(playRound(playerSelection.toLowerCase(), computerSelection.toLowerCase() ) );
+  function game() {
   
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt("Pick rock, paper, scissors");
+        let computerSelection = computerPlay();
+        console.log(playRound(playerSelection.toLowerCase(), computerSelection.toLowerCase() ) );
+     }
+     
+  }
+
+  game();
