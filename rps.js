4650107aba865ab32edc4b8a3309c171774aafb7
Rob DiVincenzo <rob.divincenzo@gmail.com>
2022-07-12 21:57:16 -0400

    Create sample round
    Create function and conditions for rps scenarios

--- rps.js
+++ rps.js
@@ -14,3 +14,44 @@ function computerPlay() {
             break;
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    // if draw, return draw
+    if( playerSelection == computerSelection ) {
+        return "Draw!";
+    }
+
+    //Rock conditions
+    if( playerSelection == "rock" ) {
+        if( computerSelection == "paper") {
+            return "You Lose! Paper beats Rock";
+        } else if( computerSelection == "scissors" ) {
+            return "You Win! Rock beats Scissors";
+        }
+    }
+    
+    //Paper conditions
+    if( playerSelection == "paper" ) {
+        if( computerSelection == "rock") {
+            return "You Win! Paper beats Rock";
+        } else if( computerSelection == "scissors" ) {
+            return "You Lose! Scissors beats Paper";
+        }
+    }
+
+    //Scissors conditions
+    if( playerSelection == "scissors" ) {
+        if( computerSelection == "rock") {
+            return "You Lose! Rock beats Scissors";
+        } else if( computerSelection == "paper" ) {
+            return "You Win! Scissors beats Paper";
+        }
+    }
+
+
+  }
+   
+  const playerSelection = "rock";
+  const computerSelection = computerPlay();
+  console.log(playRound(playerSelection.toLowerCase(), computerSelection.toLowerCase() ) );
+  
