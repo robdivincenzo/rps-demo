41b4e07f8383e5a5232efe1b4a068881686f0706
Rob DiVincenzo <rob.divincenzo@gmail.com>
2022-07-13 22:53:44 -0400

    Create basic ui
    Change prompt to DOM events

--- rps.js
+++ rps.js
@@ -25,24 +25,30 @@ function playRound(playerSelection, computerSelection) {
         //Rock conditions
         case "rock":
             if( computerSelection == "paper") {
+                computerScore++;
                 return "You Lose! Paper beats Rock";
             } else if( computerSelection == "scissors" ) {
+                playerScore++;
                 return "You Win! Rock beats Scissors";
             }    
             break;
         //Paper conditions
         case "paper":
             if( computerSelection == "rock") {
+                playerScore++;
                 return "You Win! Paper beats Rock";
             } else if( computerSelection == "scissors" ) {
+                computerScore++;
                 return "You Lose! Scissors beats Paper";
             }    
             break;
         //Scissors conditions
         case "scissors":
             if( computerSelection == "rock") {
+                computerScore++;
                 return "You Lose! Rock beats Scissors";
             } else if( computerSelection == "paper" ) {
+                playerScore++;
                 return "You Win! Scissors beats Paper";
             }    
             break;
@@ -53,15 +59,39 @@ function playRound(playerSelection, computerSelection) {
     }
 
   }
-   
+
+  function getResult(e) {
+        const results = document.querySelector('div.results');
+        const newResult = document.createElement('div');
+        newResult.innerText = playRound(this.innerText.toLowerCase(), computerPlay().toLowerCase() );
+        results.appendChild(newResult);
+        tallyScore();
+    }
+
   function game() {
-  
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("Pick rock, paper, scissors");
-        let computerSelection = computerPlay();
-        console.log(playRound(playerSelection.toLowerCase(), computerSelection.toLowerCase() ) );
-     }
-     
+     const buttons = document.querySelectorAll('button');
+     buttons.forEach( (button) => {
+        button.addEventListener('click', getResult);
+     });
   }
 
+  function tallyScore() {
+    const winner = document.querySelector('div.winner');
+    const score = document.querySelector('div.score');
+    winner.innerText = "";
+    score.innerText = `${playerScore} - ${computerScore}`;
+    if( playerScore >= 5 ) {
+        winner.innerText = "PLAYER WINS!";
+        playerScore = 0;
+        computerScore = 0;
+    }
+    if( computerScore >= 5 ) {
+        winner.innerText ="COMPUTER WINS!";
+        playerScore = 0;
+        computerScore = 0;
+    }
+  }
+
+  let playerScore = 0;
+  let computerScore = 0;
   game();
